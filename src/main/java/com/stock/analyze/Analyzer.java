4aba8e7746c7a0647aae401706d114e1f2fe35fb
Selman Ercan <ercan.s.selman@gmail.com>
2018-04-16 21:36:36 +0200

    Basic calculations using Spark.

--- src/main/java/com/stock/analyze/Analyzer.java
+++ src/main/java/com/stock/analyze/Analyzer.java
@@ -7,6 +7,8 @@ import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.apache.spark.sql.functions.*;
+
 public class Analyzer {
     private final Logger log = LoggerFactory.getLogger(Analyzer.class);
 
@@ -27,8 +29,12 @@ public class Analyzer {
                 .option("password", "stock")
                 .load();
 
-        for (Row row : jdbcDF.collectAsList()) {
-            log.info(row.toString());
-        }
+//        for (Row row : jdbcDF.collectAsList()) {
+//            log.info(String.join(",", jdbcDF.columns()));
+//            log.info(row.toString());
+//        }
+        jdbcDF.select(min("low")).show();
+        jdbcDF.select(avg("low")).show();
+        jdbcDF.select(max("low")).show();
     }
 }
