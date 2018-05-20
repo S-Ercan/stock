ab9ba9a5415a18b734574fd45f96bea6aad4965f
Selman Ercan <ercan.s.selman@gmail.com>
2018-05-20 17:45:00 +0200

    Returning list of TimeSeries objects from endpoint.

--- src/main/java/com/stock/controller/Controller.java
+++ src/main/java/com/stock/controller/Controller.java
@@ -1,12 +1,27 @@
 package com.stock.controller;
 
+import com.stock.dao.DAO;
+import com.stock.entity.TimeSeries;
+import org.hibernate.query.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class Controller {
+    private Logger log = LoggerFactory.getLogger(Controller.class);
+
     @RequestMapping("/test")
-    public String test() {
-        return "test";
+    @CrossOrigin("*")
+    public ResponseEntity<List<TimeSeries>> test() {
+        Query<TimeSeries> query = DAO.getSession().createQuery("FROM TimeSeries ORDER BY trading_day DESC").setMaxResults(10);
+        List<TimeSeries> result = query.list();
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
