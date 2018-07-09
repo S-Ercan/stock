5fc62de73f4d78885ab9ad34be058e8d3284d391
Selman Ercan <ercan.s.selman@gmail.com>
2018-07-09 20:46:20 +0200

    Added trend calculator for calculating differences between closing prices at the start and end of certain periods of time.

--- src/main/java/com/stock/StockApplication.java
+++ src/main/java/com/stock/StockApplication.java
@@ -1,17 +1,23 @@
 package com.stock;
 
-import com.stock.analyze.Analyzer;
-import com.stock.collect.Collector;
+import com.stock.analyze.TrendCalculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 @ComponentScan(basePackages = "com.stock.controller")
 @EnableAutoConfiguration
 public class StockApplication {
     public static void main(String[] args) {
         SpringApplication.run(StockApplication.class);
-        new Collector().collect();
-        new Analyzer().trainLRModel();
+//        new Collector().collect();
+//        new Analyzer().trainLRModel();
+
+        Calendar today = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        today.add(Calendar.DATE, -60);
+        new TrendCalculator().calculateTrend(today.getTime());
     }
 }
---
+++ src/main/java/com/stock/analyze/TrendCalculator.java
@@ -0,0 +1,53 @@
+package com.stock.analyze;
+
+import com.stock.dao.DAO;
+import com.stock.entity.TimeSeries;
+import org.hibernate.query.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+public class TrendCalculator {
+    private Logger log = LoggerFactory.getLogger(TrendCalculator.class);
+
+    public void calculateTrend(Date startDate) {
+        DecimalFormat priceFormat = new DecimalFormat("0.00");
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String startDateString = dateFormat.format(startDate);
+
+        Calendar today = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        Date endDate = today.getTime();
+        String endDateString = dateFormat.format(endDate);
+
+        log.info("Calculating difference between closing prices on {} and {}.", startDateString, endDateString);
+
+        Query query = DAO.getSession().createQuery("" +
+                "FROM TimeSeries " +
+                "WHERE trading_day>='" + startDateString + "' " +
+                "ORDER BY trading_day ASC"
+        ).setMaxResults(1);
+        List result = query.list();
+
+        TimeSeries timeSeriesStart = (TimeSeries) result.get(0);
+        log.info("Found entry on {} with closing price {}.", timeSeriesStart.getTradingDay().toString(), priceFormat.format(timeSeriesStart.getClosingPrice()));
+
+        query = DAO.getSession().createQuery("" +
+                "FROM TimeSeries " +
+                "WHERE trading_day<='" + endDateString + "' " +
+                "ORDER BY trading_day DESC"
+        ).setMaxResults(1);
+        result = query.list();
+
+        TimeSeries timeSeriesEnd = (TimeSeries) result.get(0);
+        log.info("Found entry on {} with closing price {}.", timeSeriesEnd.getTradingDay().toString(), priceFormat.format(timeSeriesEnd.getClosingPrice()));
+
+        log.info("Difference: {}", priceFormat.format(timeSeriesEnd.getClosingPrice() - timeSeriesStart.getClosingPrice()));
+    }
+}
